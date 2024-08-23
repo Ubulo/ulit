99fef897ef215d68cee02ede2c4506836b347dec
yves <yves@demo.com>
2024-08-23 21:10:45 +0800

    Tutorial - data fetching part - 8

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,7 +24,14 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+    try {
+      await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    } catch (error) {
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -38,18 +45,28 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
    
     const amountInCents = amount * 100;
-   
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-   
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-  }
 
-  export async function deleteInvoice(id: string) {
+    try {
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+
+    revalidatePath('/dashboard/invoices');
+    // Redirect work by throwing error - if you put it in the try/catch, redirect will be canceled.
+    // Redirect won't happen if an error is cought.
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
   }
+}
